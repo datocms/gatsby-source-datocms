9c82301559c2d6b7671579857a9f73650f8d1272
Stefano Verna <s.verna@cantierecreativo.net>
2018-02-13 10:34:29 +0100

    Fix

--- src/fetch.js
+++ src/fetch.js
@@ -2,8 +2,8 @@ const EntitiesRepo = require('datocms-client/lib/local/EntitiesRepo');
 
 module.exports = function(client) {
   return Promise.all([
-    this.client.get('/site', { include: 'item_types,item_types.fields' }),
-    this.client.items.all({}, { deserializeResponse: false, allPages: true }),
+    client.get('/site', { include: 'item_types,item_types.fields' }),
+    client.items.all({}, { deserializeResponse: false, allPages: true }),
   ])
   .then(([site, allItems]) => {
     return new EntitiesRepo(site, allItems);
