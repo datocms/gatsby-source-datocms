8e14eb8920d95383cecb333a9ffd53c482f3a263
Stefano Verna <s.verna@cantierecreativo.net>
2018-05-18 17:00:31 +0200

    Fix

--- src/fetch.js
+++ src/fetch.js
@@ -4,9 +4,9 @@ module.exports = function(client, previewMode) {
   return Promise.all([
     client.get('/site', { include: 'item_types,item_types.fields' }),
     client.items.all({ version: previewMode ? 'latest' : 'published' }, { deserializeResponse: false, allPages: true }),
-    client.uploads.all({ version: previewMode ? 'latest' : 'published' }, { deserializeResponse: false, allPages: true }),
+    client.uploads.all({}, { deserializeResponse: false, allPages: true }),
   ])
-  .then(([site, allItems]) => {
-    return new EntitiesRepo(site, allItems);
+  .then(([site, allItems, allUploads]) => {
+    return new EntitiesRepo(site, allItems, allUploads);
   });
 }
