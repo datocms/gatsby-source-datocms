4b6de229fae5e490f32330244ae0d23b5bde8a96
Stefano Verna <s.verna@cantierecreativo.net>
2018-05-30 11:55:59 +0200

    Add apiUrl option

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -12,7 +12,7 @@ const extendAssetNode = require('./extendAssetNode');
 
 exports.sourceNodes = async (
   { boundActionCreators, getNodes, hasNodeChanged, store, reporter },
-  { apiToken, disableLiveReload, previewMode }
+  { apiToken, disableLiveReload, previewMode, apiUrl }
 ) => {
   const {
     createNode,
@@ -21,13 +21,18 @@ exports.sourceNodes = async (
     setPluginStatus,
   } = boundActionCreators;
 
-  const client = new SiteClient(
-    apiToken,
-    {
-      'X-Reason': 'dump',
-      'X-SSG': 'gatsby',
-    }
-  );
+  let client;
+
+  const clientHeaders = {
+    'X-Reason': 'dump',
+    'X-SSG': 'gatsby',
+  };
+
+  if (apiUrl) {
+    client = new SiteClient(apiToken, clientHeaders, apiUrl);
+  } else {
+    client = new SiteClient(apiToken, clientHeaders);
+  }
 
   const sync = async () => {
     const nodeIds = [];
