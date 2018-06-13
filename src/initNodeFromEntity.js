879c8a3cfa920120dbdc911e7759ab1f5c39c5d1
Stefano Verna <s.verna@cantierecreativo.net>
2018-06-13 09:32:42 +0200

    Add originalId (closes #18)

--- src/initNodeFromEntity.js
+++ src/initNodeFromEntity.js
@@ -4,6 +4,7 @@ const makeType = require('./makeType');
 module.exports = function initNodeFromEntity(entity, locale = null) {
   return {
     id: mId(entity, locale),
+    originalId: entity.id,
     parent: null,
     children: [],
     internal: {
