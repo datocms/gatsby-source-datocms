d51b071c98390681444d15d39dee8fef47f85011
Stefano Verna <s.verna@cantierecreativo.net>
2018-07-24 15:57:37 +0200

    Bump

--- src/fetch.js
+++ src/fetch.js
@@ -4,7 +4,7 @@ module.exports = function(client, previewMode) {
   return Promise.all([
     client.get('/site', { include: 'item_types,item_types.fields' }),
     client.items.all({ version: previewMode ? 'latest' : 'published' }, { deserializeResponse: false, allPages: true }),
-    client.uploads.all({ 'filter[type]' => 'used' }, { deserializeResponse: false, allPages: true }),
+    client.uploads.all({ 'filter[type]': 'used' }, { deserializeResponse: false, allPages: true }),
   ])
   .then(([site, allItems, allUploads]) => {
     return new EntitiesRepo(site, allItems, allUploads);
