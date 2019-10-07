b72c71aa15b0eca838245be77b446458609e230f
Stefano Verna <s.verna@datocms.com>
2019-10-07 14:34:01 +0200

    Replace babel-polyfill

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,4 +1,5 @@
-require('babel-polyfill');
+require('core-js/stable');
+require('regenerator-runtime/runtime');
 
 const sourceNodes = require('./hooks/sourceNodes');
 exports.sourceNodes = sourceNodes;
