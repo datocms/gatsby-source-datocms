83e86a346fc7820ccbdfd9dcdfa1328dd032581d
Stefano Verna <s.verna@datocms.com>
2019-10-07 18:11:56 +0200

    v2.1.17-0

--- utils/isImage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = function (_ref) {
-  var format = _ref.format,
-      width = _ref.width,
-      height = _ref.height;
-  return ['png', 'jpg', 'jpeg', 'gif'].includes(format) && width && height;
-};
