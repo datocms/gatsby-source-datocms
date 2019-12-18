5d398a0aac45f72604899325b06b966c417936f5
Stefano Verna <s.verna@datocms.com>
2019-12-18 12:44:05 +0100

    Swap DatoCmsSeoMetaTags and DatoCmsTextNode

--- src/hooks/sourceNodes/createTypes/fieldTypes/DatoCmsTextNode.js
+++ src/hooks/sourceNodes/createTypes/fieldTypes/DatoCmsTextNode.js
@@ -1,11 +1,12 @@
 module.exports = ({ entitiesRepo, actions, schema }) => {
   actions.createTypes([
+    // We're inferring as it's not our duty to add the childMarkdownRemark field
+    // as it's added ex-post by gatsby-transform-remark!
+
     schema.buildObjectType({
-      name: 'DatoCmsSeoMetaTags',
-      extensions: { infer: false },
-      fields: {
-        tags: 'JSON',
-      },
+      name: 'DatoCmsTextNode',
+      extensions: { infer: true },
+      fields: {},
       interfaces: [`Node`],
     }),
   ]);
