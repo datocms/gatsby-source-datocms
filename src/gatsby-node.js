348ee85032e79c03556b4875314285b991c03132
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2020-03-03 13:24:15 +0530

    Remove onPreExtractQueries

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -3,6 +3,3 @@ require('regenerator-runtime/runtime');
 
 const sourceNodes = require('./hooks/sourceNodes');
 exports.sourceNodes = sourceNodes;
-
-const onPreExtractQueries = require('./hooks/onPreExtractQueries');
-exports.onPreExtractQueries = onPreExtractQueries;
