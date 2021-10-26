008822901a94df483809b13ebc8807fa3ca2e637
Daniel Lew <danielshlomolew@gmail.com>
2021-10-26 09:28:26 -0400

    add create node manifest functionality

--- src/utils.js
+++ src/utils.js
@@ -62,6 +62,52 @@ async function getLoader({ cache, loadStateFromCache, ...options }) {
   return loader;
 }
 
+const FORTY_EIGHT_HOURS = 1000 * 60 * 60 * 48; // ms * sec * min * hr
+
+const datocmsCreateNodeManifest = ({ node, context }) => {
+  try {
+    const { unstable_createNodeManifest } = context.actions;
+    const createNodeManifestIsSupported =
+      typeof unstable_createNodeManifest === `function`;
+
+    const nodeNeedsManifestCreated =
+      node?.entityPayload?.meta?.updated_at && node?.locale;
+
+    const shouldCreateNodeManifest =
+      createNodeManifestIsSupported && nodeNeedsManifestCreated;
+
+    if (shouldCreateNodeManifest) {
+      // Example manifestId: "34324203-2021-07-08T21:52:28.791+01:00"
+
+      const nodeWasRecentlyUpdated =
+        Date.now() - new Date(node.entityPayload.meta.updated_at).getTime() <=
+        // Default to only create manifests for items updated in last 48 hours
+        (process.env.CONTENT_SYNC_DATOCMS_HOURS_SINCE_ENTRY_UPDATE ||
+          FORTY_EIGHT_HOURS);
+
+      // We need to create manifests on cold builds, this prevents from creating many more
+      // manifests than we actually need
+      if (!nodeWasRecentlyUpdated) return;
+
+      const manifestId = `${node?.entityPayload?.id}-${node.entityPayload.meta.updated_at}`;
+
+      console.info(`DatoCMS: Creating node manifest with id ${manifestId}`);
+
+      unstable_createNodeManifest({
+        manifestId,
+        node,
+      });
+    } else if (!createNodeManifestIsSupported) {
+      console.warn(
+        `DatoCMS: Your version of Gatsby core doesn't support Content Sync (via the unstable_createNodeManifest action). Please upgrade to the latest version to use Content Sync in your site.`,
+      );
+    }
+  } catch (e) {
+    console.info(`Cannot create node manifest`, e.message);
+  }
+};
+
 module.exports = {
   getLoader,
+  datocmsCreateNodeManifest,
 };
