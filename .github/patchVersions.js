8cc9ee5baca25841d8f9fb11d339ffade9e761a7
Bruno Alla <alla.brunoo@gmail.com>
2022-12-05 16:06:42 +0000

    ci: extract versions patcher to a separate script

---
+++ .github/patchVersions.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const gatsbyVersion = parseInt(process.env.GATSBY_VERSION);
+const pluginImageVersion = gatsbyVersion - 3;
+const pluginSharpImageVersion = gatsbyVersion;
+const transformerRemarkVersion = gatsbyVersion + 1;
+
+packageJson = JSON.parse(fs.readFileSync('package.json'));
+
+packageJson.devDependencies.gatsby = `^${gatsbyVersion}.0.0`;
+packageJson.devDependencies[
+  'gatsby-plugin-image'
+] = `^${pluginImageVersion}.0.0`;
+packageJson.devDependencies[
+  'gatsby-plugin-sharp'
+] = `^${pluginSharpImageVersion}.0.0`;
+packageJson.devDependencies[
+  'gatsby-transformer-remark'
+] = `^${transformerRemarkVersion}.0.0`;
+packageJson.engines.node = '>=' + process.env.NODE_VERSION;
+
+fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
