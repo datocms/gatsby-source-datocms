4bddfcbff6c1a8f36a4a812f37e12d95b6be77f6
pieh <misiek.piechowiak@gmail.com>
2022-12-06 17:32:23 +0100

    test: chdir before importing anything from gatsby so main and child processes have shared cwd

--- test/support/buildQueryExecutor.js
+++ test/support/buildQueryExecutor.js
@@ -1,17 +1,21 @@
 require('regenerator-runtime/runtime');
+const path = require('path');
+const gatsbyProjectPath = path.resolve(
+  path.join(__dirname, '../fixtures/sample-gatsby-structure'),
+);
+// We are setting up CWD early, before importing any of gatsby files, so that
+// `process.cwd()` in THIS process and also in child processes spawned by gatsby
+// is actually the same. Otherwise `cache` would be backed by different DBs and
+// therefore wouldn't actually allow to share data between processes.
+process.chdir(gatsbyProjectPath);
+
 const { bootstrap } = require('gatsby/dist/bootstrap');
 const reporter = require('gatsby-cli/lib/reporter');
 const { setStore } = require('gatsby-cli/lib/reporter/redux');
-const path = require('path');
 const rimraf = require('rimraf');
 
 module.exports = async function buildQueryExecutor(apiToken) {
-  const gatsbyProjectPath = path.resolve(
-    path.join(__dirname, '../fixtures/sample-gatsby-structure'),
-  );
   rimraf.sync(path.join(gatsbyProjectPath, './.cache'));
-
-  process.chdir(gatsbyProjectPath);
 
   process.env.DATOCMS_API_TOKEN = apiToken;
 
