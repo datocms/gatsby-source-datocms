df9adc9131cdf5e713480f88359cf3dccc7d36b7
Irene Oppo <i.oppo@datocms.com>
2023-06-13 10:07:37 +0200

    test

--- errorMap.js
+++ errorMap.js
@@ -1,26 +1,6 @@
 'use strict';
 
-function _typeof(obj) {
-  '@babel/helpers - typeof';
-  return (
-    (_typeof =
-      'function' == typeof Symbol && 'symbol' == typeof Symbol.iterator
-        ? function(obj) {
-            return typeof obj;
-          }
-        : function(obj) {
-            return obj &&
-              'function' == typeof Symbol &&
-              obj.constructor === Symbol &&
-              obj !== Symbol.prototype
-              ? 'symbol'
-              : typeof obj;
-          }),
-    _typeof(obj)
-  );
-}
 function _defineProperty(obj, key, value) {
-  key = _toPropertyKey(key);
   if (key in obj) {
     Object.defineProperty(obj, key, {
       value: value,
@@ -33,24 +13,13 @@ function _defineProperty(obj, key, value) {
   }
   return obj;
 }
-function _toPropertyKey(arg) {
-  var key = _toPrimitive(arg, 'string');
-  return _typeof(key) === 'symbol' ? key : String(key);
-}
-function _toPrimitive(input, hint) {
-  if (_typeof(input) !== 'object' || input === null) return input;
-  var prim = input[Symbol.toPrimitive];
-  if (prim !== undefined) {
-    var res = prim.call(input, hint || 'default');
-    if (_typeof(res) !== 'object') return res;
-    throw new TypeError('@@toPrimitive must return a primitive value.');
-  }
-  return (hint === 'string' ? String : Number)(input);
-}
+
 var pluginPrefix = 'gatsby-source-datocms';
+
 function prefixId(id) {
   return ''.concat(pluginPrefix, '_').concat(id);
 }
+
 var ReporterLevel = {
   Error: 'ERROR',
 };
@@ -65,6 +34,7 @@ var ReporterCategory = {
 var CODES = {
   MissingAPIToken: '10000',
 };
+
 var ERROR_MAP = _defineProperty({}, CODES.MissingAPIToken, {
   text: function text(context) {
     return context.sourceMessage;
@@ -72,6 +42,7 @@ var ERROR_MAP = _defineProperty({}, CODES.MissingAPIToken, {
   level: ReporterLevel.Error,
   category: ReporterCategory.User,
 });
+
 module.exports = {
   pluginPrefix: pluginPrefix,
   CODES: CODES,
